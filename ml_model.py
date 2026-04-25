6509d2a71590f21591fe6d0fb578b59efb46418d
bushra-microbio <mumtazbushra2@gmail.com>
2026-04-25 23:01:52 +0500

    Add Random Forest model for AMR prediction

---
+++ ml_model.py
@@ -0,0 +1,27 @@
+# Machine Learning Model for Antimicrobial Resistance
+# Author: Bushra Mumtaz
+
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+
+# Sample dataset
+data = {
+    "Antibiotic": ["Ampicillin", "Ciprofloxacin", "Gentamicin", "Ceftriaxone", "Meropenem"],
+    "Resistance_Rate": [80, 45, 30, 60, 10],
+    "Resistant": [1, 1, 0, 1, 0]  # 1 = Resistant, 0 = Sensitive
+}
+
+df = pd.DataFrame(data)
+
+# Features and labels
+X = df[["Resistance_Rate"]]
+y = df["Resistant"]
+
+# Model
+model = RandomForestClassifier()
+model.fit(X, y)
+
+# Prediction
+prediction = model.predict([[50]])
+
+print("Prediction for resistance at 50% rate:", prediction)
